d65bb1dc8a7fa5b5eeea1a92aadd75f65920985b
Knumber7 <qosmos7@gmail.com>
2018-06-03 11:37:04 +0900

    first commit

---
+++ Assets/my_script/collision_script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision coli)
+    {
+        Debug.Log("collision");
+        if(coli.transform.tag == "Player")
+        {
+            coli.gameObject.GetComponent<material_change_script>().coli_flag = true;
+        }
+    }
+}
---
+++ Assets/my_script/material_change_script.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class material_change_script : MonoBehaviour {
+
+    public Material red, blue;
+    public bool coli_flag;
+    // Use this for initialization
+    void Start()
+    {
+        coli_flag = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (coli_flag)
+        {
+            GetComponent<Renderer>().material = blue;
+        }
+
+
+	}
+}
